2142946c0853a7d8c364664f9ff66900f8db20eb
Sultanxda <sultanxda@gmail.com>
2015-06-07 02:52:02 -0700

    find7op: Import blobs from CM12.1
    *Camera libraries from OxygenOS

--- find7op/find7op-vendor.mk
+++ find7op/find7op-vendor.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE_PACKAGE_OVERLAYS := vendor/oppo/find7a/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/oppo/find7op/overlay
 
 PRODUCT_PACKAGES += libtime_genoff
 
